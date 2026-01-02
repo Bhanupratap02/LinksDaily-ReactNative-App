d00b80ae5ed9cfed5d9b44d8b8ffdb6a2960b4c5
Bhanu Pratap Behera <100342013+Bhanupratap02@users.noreply.github.com>
2026-01-02 16:11:50 +0530

    Hide sensitive environment variables
    Comment out sensitive environment variables in index.js

--- linksd-server/index.js
+++ linksd-server/index.js
@@ -33,3 +33,13 @@ app.use("/api", linkRoutes);
 const port = process.env.PORT || 8000;
 
 http.listen(port, () => console.log("Server running on port 8000"));
+//.env file variables
+// DATABASE = mongodb://localhost:27017/LinksDaily
+// PORT = 8000
+// JWT_SECRET = LinksDaily
+// CLOUDINARY_NAME = da01buahx
+// CLOUDINARY_KEY = 172638542738785
+// CLOUDINARY_SECRET = 6h1JfKME4waO1wfQC9UI8tbyYUs
+// SENDGRID_KEY = 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
+// EMAIL_FROM = pallishree.testdev@gmail.com
+// CLOUDINARY_URL=cloudinary://172638542738785:6h1JfKME4waO1wfQC9UI8tbyYUs@da01buahx
